0780db7b3b09641b1390e4a6f325e39d926cd61c
tomad6909 <tomad6909@wrdsb.ca>
2020-09-14 15:29:29 +0000

    Finished A1Q2

--- Main.java
+++ Main.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program divides two numbers and gives you the remainder
+ * @author David Toma
  */
 public class Main {
-
+  
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+    
+    // ask user for the two integers on seperate lines
+    System.out.println("Please enter in two integers, on seperate lines, to divide:");
+
+    int integer1 = input.nextInt();
+    int integer2 = input.nextInt();
+
+    int answer = integer1 / integer2;
+    int remainder = integer1 % integer2;
+
+    System.out.println("The answer is " + answer + "  remainder " + remainder + ".");
+
     
   }
 }
